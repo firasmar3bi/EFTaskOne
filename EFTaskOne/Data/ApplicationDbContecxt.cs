f4e0fb86d9d84fc3c06e6b100b5ccb5e6a9ce70d
firasmar3bi <mfiras190@gmail.com>
2024-08-04 19:07:48 +0300

    first commit

---
+++ EFTaskOne/Data/ApplicationDbContecxt.cs
@@ -0,0 +1,21 @@
+﻿using EFTaskOne.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTaskOne.Data
+{
+    internal class ApplicationDbContecxt : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;Database=ASP9_EFTask;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Products> products { get; set; }
+        
+    }
+}
